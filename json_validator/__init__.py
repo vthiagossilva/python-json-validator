522a25672f9cb6ace8f9f0bcca0c26464d8ff3f2
Thiago Silva <valtemir.thiago@gmail.com>
2021-12-19 12:09:06 -0300

    .

--- json_validator/__init__.py
+++ json_validator/__init__.py
@@ -1,2 +1,2 @@
-from patterns import text, important, schema, value, choices, boolean, digits, regex, momento
-from validate_schema import __validate_schema as validate
+from json_validator.patterns import text, important, schema, value, choices, boolean, digits, regex, momento
+from json_validator.validate_schema import __validate_schema as validate
